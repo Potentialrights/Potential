0a54b2c42dca7643a59577e2e7155194c82d1249
Potentialrights <potentialrights25@gmail.com>
2025-10-24 21:43:33 +0100

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,5 @@
+
+export const GAME_DURATION = 30; // seconds
+export const MOLE_UP_MIN_TIME = 400; // milliseconds
+export const MOLE_UP_MAX_TIME = 1000; // milliseconds
+export const GRID_SIZE = 9;
